c47db38109a283a6e6ab22530ea95a5c4aaac9b6
Ulrik Røsby <ulrik@rosby.no>
2022-10-03 00:16:33 +0200

    update run file

--- tdt4290/main.py
+++ tdt4290/main.py
@@ -1,10 +1,20 @@
-from parse_lidar import parse_lidar_data
+import json
+
+import pandas as pd
+
+from .label_audio_with_lidar import label_audio_with_lidar
 
 
 def main():
-    data = parse_lidar_data("data/lidar_log_grilstad_22.09.22_10_00_13_00.txt")
-    print("Hello")
-    print(data)
+    print("Start...")
+    audio_start = pd.Timestamp("2022-09-22 10:00:02.164")
+    labeled = label_audio_with_lidar(
+        ".data/audio_22092022.WAV",
+        "data/lidar_log_grilstad_22.09.22_10_00_13_00.txt",
+        audio_start,
+    )
+    print("Done labeling")
+    print(json.dumps(labeled))
 
 
 if __name__ == "__main__":
