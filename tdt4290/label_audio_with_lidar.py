2b5b2382a9df5ff7333693ca1c8e13101f5197b4
Hans Tjøtta <hahotjo@gmail.com>
2022-10-04 13:29:18 +0200

    lidar_data as dataframe argument
    Co-authored-by: Ulrik Røsby <ulrik2204@users.noreply.github.com>
    Co-authored-by: Therese Sigmundstad <thersi@users.noreply.github.com>

--- tdt4290/label_audio_with_lidar.py
+++ tdt4290/label_audio_with_lidar.py
@@ -6,13 +6,12 @@ from .parse_lidar import parse_lidar_data
 
 
 def label_audio_with_lidar(
-    lidar_filename: str,
+    lidar: pd.DataFrame,
     audio_start: pd.Timestamp,  # = pd.to_datetime("2022-09-22 10:05:10.0000"),
     audio_sample_rate: int = 32000,
     vehicle_clip_length_s: int = 2,
     audio_length: Optional[pd.Timedelta] = None,
 ) -> pd.DataFrame:
-    lidar: pd.DataFrame = parse_lidar_data(lidar_filename)
     lidar["timedelta_start"] = lidar["datetime_enter"] - audio_start
     lidar["timedelta_end"] = lidar["datetime_leave"] - audio_start
     absolute_start = lidar["timedelta_start"].dt.total_seconds() * audio_sample_rate
