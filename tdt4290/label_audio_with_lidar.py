66a9ae64c4cc1c13ef8d8aebcf59a1728b0091a3
Ulrik Røsby <ulrik@rosby.no>
2022-10-11 12:45:42 +0200

    fix: change audio labeling from using librosa arrya index to seconds

--- tdt4290/label_audio_with_lidar.py
+++ tdt4290/label_audio_with_lidar.py
@@ -2,8 +2,6 @@ from typing import Optional
 
 import pandas as pd
 
-from .parse_lidar import parse_lidar_data
-
 
 def label_audio_with_lidar(
     lidar: pd.DataFrame,
@@ -14,14 +12,14 @@ def label_audio_with_lidar(
 ) -> pd.DataFrame:
     lidar["timedelta_start"] = lidar["datetime_enter"] - audio_start
     lidar["timedelta_end"] = lidar["datetime_leave"] - audio_start
-    absolute_start = lidar["timedelta_start"].dt.total_seconds() * audio_sample_rate
-    absolute_end = lidar["timedelta_end"].dt.total_seconds() * audio_sample_rate
+    absolute_start = lidar["timedelta_start"].dt.total_seconds()
+    absolute_end = lidar["timedelta_end"].dt.total_seconds()
     diff_half = (absolute_end - absolute_start) / 2
-    lidar["audio_start_index"] = (
-        absolute_start + diff_half - (vehicle_clip_length_s / 2) * audio_sample_rate
+    lidar["audio_start_s"] = (
+        absolute_start + diff_half - (vehicle_clip_length_s / 2)
     ).astype("int")
-    lidar["audio_end_index"] = (
-        absolute_end - diff_half + (vehicle_clip_length_s / 2) * audio_sample_rate
+    lidar["audio_end_s"] = (
+        absolute_end - diff_half + (vehicle_clip_length_s / 2)
     ).astype("int")
     if audio_length is None:
         return lidar
